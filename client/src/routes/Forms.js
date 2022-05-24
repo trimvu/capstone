5f2677ff6b87e2e196fd99f10a8342e46d3db4d7
Dane <housejanssen@gmail.com>
2022-05-24 12:55:15 -0400

    started connecting routes from back end to front end

---
+++ client/src/routes/Forms.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+const Forms = ({setUserInput}) => {
+    const [textValue, setTextValue] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUserInput(textValue)
+    
+    }
+    return (
+    <>
+    <form onSubmit={handleSubmit}>
+        <input type="text" value={textValue} onChange={(e)=> setTextValue(e.target.value)} />
+        <input type="submit" />
+    </form>
+    </>
+    )
+}
+
+export default Forms
