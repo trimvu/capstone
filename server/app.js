d24d10fff5d728913b8da11918d4d60565ff1bbb
Tri <vutriminh@gmail.com>
2022-05-27 17:56:08 -0400

    imported a path

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 let port = process.env.PORT || 3001;
 require("dotenv").config()
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(require('./routes/authentication.js'))
